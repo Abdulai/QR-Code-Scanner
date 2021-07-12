746cca5aba541d4b916918a566e0cfec09ae06b3
Abdulai <abdulbah40@hotmail.co.uk>
2021-07-12 16:56:35 +0000

    serial search page created

---
+++ lib/screens/serial_search.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SerealSearch extends StatefulWidget {
+  const SerealSearch({Key key}) : super(key: key);
+
+  @override
+  _SerealSearchState createState() => _SerealSearchState();
+}
+
+class _SerealSearchState extends State<SerealSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
